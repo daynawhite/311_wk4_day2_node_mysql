f341a29a7c9bbae93e5cefe12cb9fc7dfb41a3c1
daynawhite <dayna26.2@gmail.com>
2024-01-23 14:11:46 -0600

    changed mysql to mysql2 where needed and created environment variables

--- sql/connection.js
+++ sql/connection.js
@@ -1,4 +1,5 @@
-const mysql = require('mysql')
+const mysql = require('mysql2')
+require('dotenv').config()
 
 class Connection {
   constructor() {
@@ -6,10 +7,10 @@ class Connection {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
       })
 
       return this.pool
